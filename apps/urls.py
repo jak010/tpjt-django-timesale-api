ddd3bbf02456296d337b371a5ed572871626a36f
jak010 <bluetoon@naver.com>
2023-06-04 22:45:55 +0900

    - mod, greeting view

--- apps/urls.py
+++ apps/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url  # django 4.x 버전 부터는 deprecated 처리
+from django.urls import path
 
 from .views import (
     greeting
 )
 
 urlpatterns = [
-    url("^$", greeting.GreetingView.as_view()),
+    path("^$", greeting.GreetingView.as_view()),
 ]
